5a40b908e1ec437f6110ecfcc6e12cd7de29645a
hcli <hcli@localhost>
2024-11-28 15:50:47 -0600

    Update grow-rootfs.sh

--- grow-rootfs.sh
+++ grow-rootfs.sh
@@ -4,7 +4,14 @@ DEV="/dev/sda"
 ROOTPART_ID=1
 ROOTPART="$DEV""$ROOTPART_ID"
 
+sudo swapoff -a
+sudo umount /dev/sda4
+
+# Remove partition 3 & 4 first
+sudo parted $DEV resizepart 3 
+sudo parted $DEV resizepart 4
+
 sudo parted $DEV resizepart 1 100%
-# Yes, End
+# Type under prompt: Yes, 480GB
 
 sudo resize2fs $ROOTPART
